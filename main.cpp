fa7a71036003020c71e0cf6e59a0e141aab9fae9
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2024-01-15 10:51:24 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+#include <thread>
+
+void Print(int num) {
+	std::cout << "Thread " << num << std::endl;
+}
 
 int main() {
 
+	std::thread th1(Print, 1);
+	th1.join();
+	std::thread th2(Print, 2);
+	th2.join();
+	std::thread th3(Print, 3);
+	th3.join();
 
 	return 0;
 }
